a1788368000e601766ff322c114a75e5050dfa93
marceloborba <marceloborba@frwk.com.br>
2023-07-14 17:52:30 -0300

    starting project

---
+++ src/main/java/com/ninvit/springbootkafkarealdata/SpringbootKafkaRealdataApplication.java
@@ -0,0 +1,13 @@
+package com.ninvit.springbootkafkarealdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaRealdataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootKafkaRealdataApplication.class, args);
+    }
+
+}
